284128cd78ff808e6fe92d5e6c7438be6ffaa4f5
Adash Dhakad <dhakadadarsh1@gmail.com>
2024-02-15 15:21:13 +0530

    removed unused variable from entities

--- network/src/main/java/com/devrev/network/room/entity/PopularMovieEntity.kt
+++ network/src/main/java/com/devrev/network/room/entity/PopularMovieEntity.kt
@@ -7,20 +7,9 @@ import androidx.room.PrimaryKey
 data class PopularMovieEntity(
     @PrimaryKey
     val id: Int,
-    val isAdultOnly: Boolean=false,
-    val popularity: Double=0.0,
     val voteAverage: Double=0.0,
-    val voteCount: Int=0,
-    val image: String? = null,
-    val backdropImage: String? = null,
     val poster_path:String? = null,
     val title: String? = null,
-    val overview: String,
-    val releaseDate: String? = null,
-    val releaseDateAlternative: String? = null,
-    val originalTitle: String? = null,
-    val originalTitleAlternative: String? = null,
-    val originalLanguage: String = "en-US",
     val nextKey:Int = 2,
     val createdAt:Int = 0
 )
